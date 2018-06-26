9df3ccc5cf08db48efc3a6f8a13e501025807bf6
Ryan Zlock <rwzlock@gmail.com>
2018-06-26 13:24:43 -0700

    Added filtering by job role.

--- static/jobs.js
+++ static/jobs.js
@@ -1,12 +1,11 @@
 var dropdown_url = "/jobrole";
+var default_url = "/jobs/Human_Resources"
 //Select the dropdown
 var selector = document.getElementById('selDataset');
 //Creating Dropdown
 Plotly.d3.json(dropdown_url, function(error, response) {
     if (error) return console.warn(error);
     var data = [response];
-    console.log(data)
-    console.log(data[0]["x"])
     //Jobs contains a list of all job names in the data
     var jobs = data[0]["x"]
     for(var i=0;i<jobs.length-1;i++) {
@@ -19,31 +18,56 @@ Plotly.d3.json(dropdown_url, function(error, response) {
         currentOption.value = "/jobs/"+jobs[i].replace(" ", "_");
         selector.appendChild(currentOption);
     }
-
-
-    //Layouts for plotting all four graphs
+})
+Plotly.d3.json(default_url, function(error, response) {
+    if (error) return console.warn(error);
+    var data = [response];
+    console.log(data)
+    console.log(data[0][0])
     var layout1 = { margin: { t: 30, b:100 },
+               title: "Gender by Job Role",
+               xaxis: { title: "Gender"},
+               yaxis: { title: "Number of Employees"}            
+    }
+    var layout2 = { margin: { t: 30, b:100 },
+               title: "Age by Job Role",
+               xaxis: { title: "Age Range"},
+               yaxis: { title: "Number of Employees"}            
+    }
+    var layout3 = { margin: { t: 30, b:100 },
+               title: "Department by Job Role",
+               xaxis: { title: "Department"},
+               yaxis: { title: "Number of Employees"}            
+    }
+    Plotly.newPlot("pie", [data[0][0]], layout1)
+    Plotly.newPlot("bar", [data[0][1]], layout2)
+    Plotly.newPlot("pie2", [data[0][2]], layout3)
+})
+
+function plotGraphs(graph_url) {
+    Plotly.d3.json(graph_url, function(error, response) {
+        if (error) return console.warn(error);
+        var data = [response];
+        console.log(data)
+        console.log(response)
+
+        var layout1 = { margin: { t: 30, b:100 },
                    title: "Gender by Job Role",
                    xaxis: { title: "Gender"},
                    yaxis: { title: "Number of Employees"}            
-    }
-    var layout2 = { margin: { t: 30, b:100 },
+        }
+        var layout2 = { margin: { t: 30, b:100 },
                    title: "Age by Job Role",
                    xaxis: { title: "Age Range"},
                    yaxis: { title: "Number of Employees"}            
-    }
-    var layout3 = { margin: { t: 30, b:100 },
+        }
+        var layout3 = { margin: { t: 30, b:100 },
                    title: "Department by Job Role",
                    xaxis: { title: "Department"},
                    yaxis: { title: "Number of Employees"}            
-    }
-    var layout4 = { margin: { t: 30, b:100 },
-                   title: "Department by Job Role",
-                   xaxis: { title: "Department"},
-                   yaxis: { title: "Number of Employees"}            
-    }
-    Plotly.plot("pie", data[0], layout1)
-    Plotly.plot("bar", data[1], layout2)
-    Plotly.plot("pie", data[2], layout3)
-    Plotly.plot("pie", data[3], layout4)
-})
+        }
+        Plotly.newPlot("pie", [data[0][0]], layout1)
+        Plotly.newPlot("bar", [data[0][1]], layout2)
+        Plotly.newPlot("pie2", [data[0][2]], layout3)
+    })
+}
