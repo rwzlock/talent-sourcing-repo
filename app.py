bb5a93e618865c0d5d12b42109c179f8a20b01d6
Ryan Zlock <rwzlock@gmail.com>
2018-07-01 22:09:28 -0700

    Added testing section.

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def jobstatistics(jobrole):
 
 #Run the app. debug=True is essential to be able to rerun the server any time changes are saved to the Python file
 if __name__ == "__main__":
-    app.run(debug=True, port=5012)
+    app.run(debug=True, port=5020)
--- static/__tests__/tests.js
+++ static/__tests__/tests.js
@@ -1,46 +1,54 @@
-var test = require('tape')
-var jobsendpoint = "/jobsummary"
-var joburl = "/jobs/Healthcare_Representative"
 
-httpGetAsync(joburl);
-//testFunc(joburl);
+var XMLHttpRequest = require('xhr2');
+var test = require('tape');
+var port = 5019;
+var endpoint = '/jobs/Healthcare_Representative';
+console.log("Hi")
 
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
-
-function testFunc(joburl) {
-
-  var jobresponse = {
+var jobresponse = {
   "0": {
         "labels": ["Male","Female"],
         "type": "pie",
-        "values": [0,0]
+        "values": [80,51]
       },
   "1": {
         "type": "bar",
         "x": ["0-19","20-27","28-35","36-43","44-51","52-60"],
-        "y": [0,0,0,0,0,0]
+        "y": [0,4,42,44,28,13]
        },
   "2": {
-        "labels": [],
+        "labels": ['Research & Development'],
         "type": "pie",
-        "values": []
+        "values": [131]
        }
   }
 
-  test('should return -1 when the value is not present in Array', function (t) {
-    t.equal(joburl, jobresponse)
+
+function handleEvent(event) {
+  //console.log(event)
+  //console.log(event.target.response)
+  var result = event.target.response;
+  var result1 = result.replace(/\n/g, "");
+  var jsonresult1 = JSON.parse(result1)
+  console.log(jsonresult1);
+  //console.log(result);
+  test('test equality of jsons', function (t) {
+    t.equal(jsonresult1, jobresponse)
     t.end();
   });
-}
+};
+
+
+
+var xhr = new XMLHttpRequest();
+xhr.addEventListener("load", handleEvent);
+//xhr.addEventListener("progress", handleEvent);
+//xhr.addEventListener("error", handleEvent);
+//xhr.addEventListener("abort", handleEvent);
+xhr.open('GET', 'http://localhost:'+port+endpoint, true);
+xhr.send(null);
+
+
 
 
 
